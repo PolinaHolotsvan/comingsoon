d01eb0fa91dcee6f916458d7f73160041e3b163f
Polina Holotsvan <swan.polina00@gmail.com>
2023-02-01 16:45:09 +0200

    Initial commit

---
+++ src/test/java/com/comingsoon/comingsoon/ComingsoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comingsoon.comingsoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComingsoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
